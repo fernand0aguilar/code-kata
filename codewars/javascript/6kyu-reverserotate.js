4e1a4ab31e0d5b4e660bee0f8c84508b255e6dff
Fernando Aguilar <fernando@aguilar.net.br>
2018-11-20 12:37:47 -0300

    Refactoring reverse rotate

--- codewars/javascript/6kyu-reverserotate.js
+++ codewars/javascript/6kyu-reverserotate.js
@@ -3,26 +3,37 @@ function revrot(str, sz) {
     if (sz <= 0 || str == "") return toReturn
     if (sz > str.length) return toReturn
 
-    let chunks = str.match(new RegExp('.{1,' + sz + '}', 'g'));
-    if (chunks[chunks.length - 1].length < sz) {
-        chunks.pop();
-    }
+    let chunks = str.match(new RegExp('.{1,' + sz + '}', 'g')).filter(word => word.length >= sz);
 
-    console.log(chunks)
-    for (let i = 0; i < chunks.length; i++) {
-        let chunk = chunks[i];
+    chunks.forEach(chunk => {
         if (sumDigits(chunk) % 2 === 0) {
             chunk = chunk.split("").reverse().join("");
         } else {
             chunk = chunk.substring(chunk.length, 1) + chunk[0];
         }
         toReturn += chunk;
-    }
+    });
     return toReturn;
 }
 
 function sumDigits(n) {
-    const array1 = n.split("").map(x => x ** 3);
-    const reducer = (accumulator, currentValue) => accumulator + currentValue;
-    return array1.reduce(reducer)
+    return n.split("")
+        .map(x => x ** 3)
+        .reduce((accumulator, currentValue) => accumulator + currentValue)
+};
+
+// OPTIMIZED
+
+function revrot(str, sz) {
+    if (sz < 1 || sz > str.length)
+        return '';
+
+    let reverse = s => s.split('').reverse().join('');
+    let rotate = s => s.slice(1) + s.slice(0, 1);
+    let sum_cubes = c => c.split('').reduce((a, b) => a + +b ** 3, 0);
+
+    return str
+        .match(new RegExp('.{' + sz + '}', 'g'))
+        .map(c => sum_cubes(c) % 2 ? rotate(c) : reverse(c))
+        .join('');
 }
